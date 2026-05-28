c582019dd87d2ff386b41f3500c660f9cdfcbb26
Rodrigo Alencar <Rodrigoalencar.mendes@gmail.com>
2026-05-28 11:10:50 -0400

    Add project files.

---
+++ Model/Categoria.cs
@@ -0,0 +1,19 @@
+﻿namespace Controle_de_Estoque.Model
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public String tipo { get; set; }
+
+        public Categoria(int id, string tipo)
+        {
+            Id = id;
+            this.tipo = tipo;
+        }
+
+        public Categoria()
+        {
+        }
+
+    }
+}
---
+++ Model/Produto.cs
@@ -0,0 +1,24 @@
+﻿namespace Controle_de_Estoque.Model
+{
+    public class Produto
+    {
+        public String nome { get; set; }
+        public float preco { get; set; }
+        public int quantidade { get; set; }
+        public Categoria categoria { get; set; }
+
+
+        public Produto(string nome, float preco, int quantidade, Categoria categoria)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.quantidade = quantidade;
+            this.categoria = categoria;
+        }
+
+        public Produto()
+        {
+        }
+
+    }
+}
